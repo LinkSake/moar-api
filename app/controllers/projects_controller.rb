be741b63f3c3da69ce9d28b38169ae7d02660a1c
Luis Angel Ortega <angel-ortega12@hotmail.com>
2021-09-20 11:48:50 -0600

    ✏️ Fixed typo in create project method

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -17,7 +17,7 @@ class ProjectsController < ApplicationController
   def create
     @project = Project.new(project_params)
 
-    if @project.saveGet 
+    if @project.save 
       render json: @project, status: :created, location: @project
     else
       render json: @project.errors, status: :unprocessable_entity
